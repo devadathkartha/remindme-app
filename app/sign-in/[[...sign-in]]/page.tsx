929da3cfad4bcbf4083f208d86122e5826c65e89
devadathkartha <devadathkartha@gmail.com>
2025-05-24 22:07:20 +0530

    dev phase

---
+++ app/sign-in/[[...sign-in]]/page.tsx
@@ -0,0 +1,9 @@
+import { SignIn } from '@clerk/nextjs'
+
+export default function Page() {
+    return( 
+        <div className='flex w-[100vw] h-[100vh] justify-center items-center bg-gray-800'>
+            <SignIn />
+        </div>
+    );
+}
